b2b7cad7b910edaadc590c496dda8720c3174dd7
Sugarsnuske <rhansen.wi@gmail.com>
2017-05-19 10:12:42 +0200

    Updated project

---
+++ aspnet_exam_movie/Model/Entities/MovieItem.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_exam_movie.Model.Entities
+{
+    public class MovieItem
+    {
+        public int MovieItemID {get;set;}
+        /*public string Task{get;set;}
+        public Boolean IsComplete{get;set;}*/
+
+        public string MovieName{get;set;}
+    }
+}
