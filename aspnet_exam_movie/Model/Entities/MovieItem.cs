529f0486672cb2df9def35486034b4999d909fcc
Sugarsnuske <rhansen.wi@gmail.com>
2017-05-19 11:10:56 +0200

    updated again

--- aspnet_exam_movie/Model/Entities/MovieItem.cs
+++ aspnet_exam_movie/Model/Entities/MovieItem.cs
@@ -7,10 +7,14 @@ namespace aspnet_exam_movie.Model.Entities
 {
     public class MovieItem
     {
-        public int MovieItemID {get;set;}
-        /*public string Task{get;set;}
-        public Boolean IsComplete{get;set;}*/
+        public int MovieItemID{get;set;}
+        public string MovieTitle{get;set;}
+        public DateTime YearPublished{get;set;}
+        public string Genre{get;set;}
+        public string Director{get;set;}
+        public TimeSpan MovieLength{get;set;}
+        public string MoviePlot{get;set;}
+        public int Rating{get;set;}
 
-        public string MovieName{get;set;}
     }
 }
